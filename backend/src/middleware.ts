5509978adaae33061be2cb4737dbc286078719e3
Mayankax <mayankbansal818@gmail.com>
2025-10-14 15:25:28 +0530

    little changes

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,7 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-
 export const userMiddleware = (req: Request,res: Response,next: NextFunction)=>{
     const header= req.headers['authorization'];
     const decoded = jwt.verify(header as string,process.env.JWT_SECRET!);
